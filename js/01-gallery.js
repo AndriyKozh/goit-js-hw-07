07273cfda0303a7d4f5f4498eef4c3cfa2e239b7
AndriyKozh <max190716@ukr.net>
2022-07-29 15:26:41 +0300

    err

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleriItem = addImegViport(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleriItem);
+
+function addImegViport(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+galleryEl.addEventListener("click", onClickImegesItem);
+
+function onClickImegesItem(ev) {
+  ev.preventDefault();
+
+  if (ev.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${ev.target.dataset.source}" width="800" height="600">
+
+`);
+
+  instance.show();
+
+  galleryEl.addEventListener("keydown", (ev) => {
+    if (ev.code === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const galleriItem = addImegViport(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleriItem);
 
-console.log(galleryItems);
+function addImegViport(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+galleryEl.addEventListener("click", onClickImegesItem);
+
+function onClickImegesItem(ev) {
+  ev.preventDefault();
+
+  if (ev.target.nodeName !== "IMG") {
+    return;
+  }
+  let lightbox = new SimpleLightbox(".gallery a", {
+    fadeSpeed: "250",
+    scrollZoom: "true",
+    animationSpeed: "250",
+  });
+
+  lightbox.show();
+
+  galleryEl.addEventListener("keydown", (ev) => {
+    if (ev.code === "Escape") {
+      lightbox.close();
+    }
+  });
+}
